c55c1cdc9f3f272f636b33dfa7bf251fa583c30f
quterma <dshapiro921@gmail.com>
2024-12-09 16:50:13 +0200

    Add API endpoints

--- packages/client/src/services/api.ts
+++ packages/client/src/services/api.ts
@@ -5,5 +5,47 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:1337/api',
   }),
-  endpoints: () => ({}),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => '/products',
+    }),
+    getProductById: builder.query({
+      query: (id: string) => `/products/${id}`,
+    }),
+
+    registerUser: builder.mutation({
+      query: (data) => ({
+        url: '/auth/local/register',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    loginUser: builder.mutation({
+      query: (data) => ({
+        url: '/auth/local',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+
+    getUser: builder.query({
+      query: () => '/users/me?populate=cart',
+    }),
+    updateCart: builder.mutation({
+      query: (data) => ({
+        url: '/users/me',
+        method: 'PUT',
+        body: data,
+      }),
+    }),
+  }),
 });
+
+export const {
+  useGetProductsQuery,
+  useGetProductByIdQuery,
+  useRegisterUserMutation,
+  useLoginUserMutation,
+  useGetUserQuery,
+  useUpdateCartMutation,
+} = api;
